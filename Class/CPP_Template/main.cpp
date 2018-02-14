f9a397703256e28dea89ff220c3ca8b4d5241ff1
mlehrjava <mark.lehr@rcc.edu>
2018-02-14 09:35:32 -0800

    Signed-off-by: mlehrjava <mark.lehr@rcc.edu>

---
+++ Class/CPP_Template/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr Mark E. Lehr
+ * Created on February 13, 2018, 1:20 PM
+ * Purpose:  Create a CSC/CIS 5 Template
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Process/Map inputs to outputs
+    
+    //Output data
+    cout<<"Hello World"<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
