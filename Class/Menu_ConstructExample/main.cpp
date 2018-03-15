33caaf18948bf2d088826295b76d7ffbd916b897
ml1150258 <mark.lehr@rcc.edu>
2018-03-15 12:47:45 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Class/Menu_ConstructExample/main.cpp
+++ Class/Menu_ConstructExample/main.cpp
@@ -34,18 +34,86 @@ int main(int argc, char** argv) {
         switch(choice){
             case '1':{
                 cout<<"Independent-If Example"<<endl;
+                //Declare variables
+                float hrsWrkd,payRate,payChk;
+                //Initialize Variables
+                cout<<"This program calculates your payCheck"<<endl;
+                cout<<"Input the hours you worked for the week "<<endl;
+                cout<<"and your Pay Rate $'s/hr"<<endl;
+                cin>>hrsWrkd>>payRate;
+                //Calculate your pay check
+                payChk=hrsWrkd*payRate;
+                //If you worked overtime > 40 hours
+                if(hrsWrkd>40)payChk+=((hrsWrkd-40)*payRate);
+                //Output all conditions
+                cout<<"Hours Worked = "<<hrsWrkd<<endl;
+                cout<<"Pay Rate = $"<<payRate<<"/hr"<<endl;
+                cout<<"Pay Check = $"<<payChk<<endl;
                 break;
             }
             case '2':{
                 cout<<"Dependent-If Example"<<endl;
+                //Declare variables
+                float hrsWrkd,payRate,payChk;
+                //Initialize Variables
+                cout<<"This program calculates your payCheck"<<endl;
+                cout<<"Input the hours you worked for the week "<<endl;
+                cout<<"and your Pay Rate $'s/hr"<<endl;
+                cin>>hrsWrkd>>payRate;
+                //Calculate your pay check
+                if(hrsWrkd>=0&&hrsWrkd<=40){
+                    payChk=hrsWrkd*payRate;
+                }else{
+                    payChk=hrsWrkd*payRate+(hrsWrkd-40)*payRate;
+                }
+                //Output all conditions
+                cout<<"Hours Worked = "<<hrsWrkd<<endl;
+                cout<<"Pay Rate = $"<<payRate<<"/hr"<<endl;
+                cout<<"Pay Check = $"<<payChk<<endl;
                 break;
             }
             case '3':{
                 cout<<"Ternary Op Example"<<endl;
+                //Declare variables
+                float hrsWrkd,payRate,payChk;
+                //Initialize Variables
+                cout<<"This program calculates your payCheck"<<endl;
+                cout<<"Input the hours you worked for the week "<<endl;
+                cout<<"and your Pay Rate $'s/hr"<<endl;
+                cin>>hrsWrkd>>payRate;
+                //Calculate your pay check
+                payChk=(hrsWrkd>=0&&hrsWrkd<=40)?
+                        hrsWrkd*payRate:
+                        hrsWrkd*payRate+(hrsWrkd-40)*payRate;
+                //Output all conditions
+                cout<<"Hours Worked = "<<hrsWrkd<<endl;
+                cout<<"Pay Rate = $"<<payRate<<"/hr"<<endl;
+                cout<<"Pay Check = $"<<payChk<<endl;
                 break;
             }
             case '4':{
                 cout<<"Switch-Case Example"<<endl;
+                                //Declare variables
+                float hrsWrkd,payRate,payChk;
+                //Initialize Variables
+                cout<<"This program calculates your payCheck"<<endl;
+                cout<<"Input the hours you worked for the week "<<endl;
+                cout<<"and your Pay Rate $'s/hr"<<endl;
+                cin>>hrsWrkd>>payRate;
+                //Calculate your pay check
+                switch(hrsWrkd>=0&&hrsWrkd<=40){
+                    case true:{
+                        payChk=hrsWrkd*payRate;
+                        break;
+                    }
+                    default:{
+                        payChk=hrsWrkd*payRate+(hrsWrkd-40)*payRate;
+                    }
+                }
+                //Output all conditions
+                cout<<"Hours Worked = "<<hrsWrkd<<endl;
+                cout<<"Pay Rate = $"<<payRate<<"/hr"<<endl;
+                cout<<"Pay Check = $"<<payChk<<endl;
                 break;
             }
         }
